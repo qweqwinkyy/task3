488f907c86e2fb94e7db33c6a7ddde3df33e0fef
anuaruspanov <anuaruspanov362@mail.ru>
2025-04-01 09:16:48 +0500

    Добавлена проверка при удалении задачи и рефакторинг методов

--- task_manager.py
+++ task_manager.py
@@ -3,12 +3,22 @@ class TaskManager:
         self.tasks = {}
 
     def add_task(self, task):
-        self.tasks[task] = "не выполнено"
-        
+        """Добавляет задачу в список с состоянием 'не выполнено'."""
+        if task not in self.tasks:
+            self.tasks[task] = "не выполнено"
+        else:
+            print(f"Задача '{task}' уже существует.")
+
     def remove_task(self, task):
+        """Удаляет задачу, если она существует. Иначе выводит сообщение об ошибке."""
         if task in self.tasks:
             del self.tasks[task]
+        else:
+            print(f"Задача '{task}' не найдена.")
 
     def mark_completed(self, task):
+        """Меняет статус задачи на 'выполнено', если задача существует."""
         if task in self.tasks:
             self.tasks[task] = "выполнено"
+        else:
+            print(f"Задача '{task}' не найдена.")
